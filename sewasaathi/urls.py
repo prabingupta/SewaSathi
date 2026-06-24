789809ee2dff7bb0cbd50f7daea5385dd33ba759
prabingupta <prabingupta567@gmail.com>
2026-06-24 14:23:39 +0545

    Fix: move uploads into proper MEDIA_ROOT, remove accidentally-committed media file from git, clean .gitignore

--- sewasaathi/urls.py
+++ sewasaathi/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from core.views import home_view
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('providers/', include('providers.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
